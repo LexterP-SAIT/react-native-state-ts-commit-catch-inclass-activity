c1dcf058b8dde188d6be4053f9e1b954031e2701
Ashlyn Knox <ashx3s@tuta.io>
2026-09-23 18:25:34 -0600

    starter code with comments

---
+++ components/TodoSection.jsx
@@ -0,0 +1,38 @@
+import { StyleSheet, View } from "react-native";
+import { TodoInput } from "./TodoInput";
+import { TodoList } from "./TodoList";
+import { generateId } from "../utils/generateId";
+
+export function TodoSection() {
+  // TODO (state): Declare todos as state
+  const todos = [
+    { id: "1", text: "Learn useState" },
+    {
+      id: "2",
+      text: "Practice typescript refactoring",
+    },
+  ];
+
+  const addTodoItem = (title) => {
+    const newTodo = { id: generateId(), title };
+    // this is an industry standard pattern
+    // setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
+
+  const deleteTodoItem = (id) => {
+    // setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <View style={styles.todoSection}>
+      <TodoInput onAddTodo={addTodoItem} />
+      <TodoList todos={todos} onDeleteItem={deleteTodoItem} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  todoSection: {
+    padding: 20,
+  },
+});
